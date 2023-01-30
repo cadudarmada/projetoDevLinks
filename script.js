b8fec55a0bcfe6e7603de89a5f1e352317847104
cadudarmada <cadudarmada@gmail.com>
2023-01-30 20:14:02 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar_light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Carlos Eduardo, usando camisa e fundo preto"
+    )
+  }
+}
